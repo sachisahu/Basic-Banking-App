747cf1a5c1d4a39536627ac16f3d47bc4361a336
Sachidanand Sahu <sachisahu0501@gmail.com>
2023-03-06 02:40:44 +0530

    first commit

---
+++ app/src/main/java/com/example/basicbankingapp/DB/UserHelper.java
@@ -0,0 +1,82 @@
+package com.example.basicbankingapp.DB;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import com.example.basicbankingapp.DB.UserContract.UserEntry;
+import com.example.basicbankingapp.Data.User;
+
+public class UserHelper extends SQLiteOpenHelper {
+
+    String TABLE_NAME = UserEntry.TABLE_NAME;
+
+
+    private static final String DATABASE_NAME = "User.db";
+
+
+    private static final int DATABASE_VERSION = 1;
+
+    public UserHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // Create a String that contains the SQL statement to create the pets table
+        String SQL_CREATE_USER_TABLE =  "CREATE TABLE " + UserEntry.TABLE_NAME + " ("
+                + UserEntry.COLUMN_USER_ACCOUNT_NUMBER + " INTEGER, "
+                + UserEntry.COLUMN_USER_NAME + " VARCHAR, "
+                + UserEntry.COLUMN_USER_EMAIL + " VARCHAR, "
+                + UserEntry.COLUMN_USER_IFSC_CODE + " VARCHAR, "
+                + UserEntry.COLUMN_USER_PHONE_NO + " VARCHAR, "
+                + UserEntry.COLUMN_USER_ACCOUNT_BALANCE + " INTEGER NOT NULL);";
+
+        // Execute the SQL statement
+        db.execSQL(SQL_CREATE_USER_TABLE);
+
+        // Insert Into Table
+        db.execSQL("insert into " + TABLE_NAME + " values(1220,'Sachidanand Sahu', 'sachisahu@gmail.com','3434','63777772733', 1500000)");
+        db.execSQL("insert into " + TABLE_NAME + " values(1221,'Sachi', 'sachi@gmail.com','3434','6377777787', 60000)");
+        db.execSQL("insert into " + TABLE_NAME + " values(1222,'Jay', 'Jay@gmail.com','3434','63779098787', 300000)");
+
+
+
+    }
+
+    public void insertUser(SQLiteDatabase db,int acno,String name,String email,String ifsc,String phone,int amt){
+        db.execSQL("Insert into "+TABLE_NAME+ " values("+acno+","+name+","+email+","+ifsc+","+phone+","+amt+")");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        if (oldVersion != newVersion) {
+            // Simplest implementation is to drop all old tables and recreate them
+            db.execSQL("DROP TABLE IF EXISTS " + UserEntry.TABLE_NAME);
+            onCreate(db);
+        }
+    }
+
+    public Cursor readAllData() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery("select * from " + UserEntry.TABLE_NAME, null);
+        return cursor;
+    }
+
+    public Cursor readParticularData (int accountNo) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery("select * from " + UserEntry.TABLE_NAME + " where " +
+                                        UserEntry.COLUMN_USER_ACCOUNT_NUMBER + " = " + accountNo, null);
+        return cursor;
+    }
+
+    public void updateAmount(int accountNo, int amount) {
+        Log.d ("TAG", "update Amount");
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.execSQL("update " + UserEntry.TABLE_NAME + " set " + UserEntry.COLUMN_USER_ACCOUNT_BALANCE + " = " + amount + " where " +
+                UserEntry.COLUMN_USER_ACCOUNT_NUMBER + " = " + accountNo);
+    }
+}
